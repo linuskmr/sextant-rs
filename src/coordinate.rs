02d81c67df9685d6aa9a425fe8ac1bb999587f55
linuskmr <linuskmr.dev@gmail.com>
2024-08-17 14:23:36 +0200

    Add longitude calculation

--- src/coordinate.rs
+++ src/coordinate.rs
@@ -1,22 +1,86 @@
+//! Latitude, Longitude, and Coordinate types.
+
 use core::fmt;
 
 use crate::{
-    angle::{self, DegreeMinutesSeconds},
+    angle::{self, Angle, DegreeMinutesSeconds},
     hemisphere,
 };
 
-/// Specifies the **north–south position** of a point on the Earth's surface.
+/// *North–south* position of a point on the Earth's surface.
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Latitude {
-    pub hemisphere: hemisphere::Latitude,
-    pub angle: angle::Angle,
+    /// The raw angle is positive for the [northern](hemisphere::Latitude::North) and negative for the [southern](hemisphere::Latitude::South) hemisphere.
+    pub raw_angle: angle::Angle,
 }
 
-/// Specifies the **east–west position** of a point on the Earth's surface.
+/// *East–west* position of a point on the Earth's surface.
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Longitude {
-    pub hemisphere: hemisphere::Longitude,
-    pub angle: angle::Angle,
+    /// The raw angle is positive for the [eastern](hemisphere::Longitude::East) and negative for the [western](hemisphere::Longitude::West) hemisphere.
+    pub raw_angle: angle::Angle,
+}
+
+/// *Geographic position* of a point on the Earth's surface using [`Latitude`] and [`Longitude`].
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub struct Coordinate {
+    /// North–south position
+    latitude: Latitude,
+    /// East–west position
+    longitude: Longitude,
+}
+
+
+impl Latitude {
+    pub fn new(raw_angle: angle::Angle) -> Self {
+        Self { raw_angle }
+    }
+
+    /// Returns the (always positive) angle of the latitude in the corresponding [`hemisphere`](Self::hemisphere).
+    pub fn angle(&self) -> angle::Angle {
+        if angle::Degrees::from(self.raw_angle).0 < 0.0 {
+            Angle::from(angle::Degrees(angle::Degrees::from(self.raw_angle).0.abs()))
+        } else {
+            self.angle()
+        }
+    }
+
+    /// Returns whether the latitude is in the [northern](hemisphere::Latitude::North) or the [southern](hemisphere::Latitude::South) hemisphere.
+    /// 
+    /// [Northern hemisphere](hemisphere::Latitude::North) for positive [`Self::raw_angle`] and [southern hemisphere](hemisphere::Latitude::South) for negative values.
+    pub fn hemisphere(&self) -> hemisphere::Latitude {
+        if angle::Degrees::from(self.raw_angle).0 < 0.0 {
+            hemisphere::Latitude::South
+        } else {
+            hemisphere::Latitude::North
+        }
+    }
+}
+
+impl Longitude {
+    pub fn new(raw_angle: angle::Angle) -> Self {
+        Self { raw_angle }
+    }
+
+    /// Returns the (always positive) angle of the longitude in the corresponding [`hemisphere`](Self::hemisphere).
+    pub fn angle(&self) -> angle::Angle {
+        if angle::Degrees::from(self.raw_angle).0 < 0.0 {
+            Angle::from(angle::Degrees(angle::Degrees::from(self.raw_angle).0.abs()))
+        } else {
+            self.raw_angle
+        }
+    }
+
+    /// Returns whether the longitude is in the [eastern](hemisphere::Longitude::East) or the [western](hemisphere::Longitude::West) hemisphere.
+    ///
+    /// [Western hemisphere](hemisphere::Longitude::West) for positive [`Self::raw_angle`] and [eastern hemisphere](hemisphere::Longitude::East) for negative values.
+    pub fn hemisphere(&self) -> hemisphere::Longitude {
+        if angle::Degrees::from(self.raw_angle).0 < 0.0 {
+            hemisphere::Longitude::West
+        } else {
+            hemisphere::Longitude::East
+        }
+    }
 }
 
 impl fmt::Display for Latitude {
@@ -24,8 +88,8 @@ impl fmt::Display for Latitude {
         write!(
             f,
             "{}\n{}",
-            self.hemisphere,
-            DegreeMinutesSeconds::from(self.angle)
+            self.hemisphere(),
+            DegreeMinutesSeconds::from(self.angle())
         )
     }
 }
@@ -35,16 +99,10 @@ impl fmt::Display for Longitude {
         write!(
             f,
             "{}\n{}",
-            self.hemisphere,
-            DegreeMinutesSeconds::from(self.angle)
+            self.hemisphere(),
+            DegreeMinutesSeconds::from(self.angle())
         )
     }
-}
-
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
-pub struct Coordinate {
-    latitude: Latitude,
-    longitude: Longitude,
 }
 
 impl fmt::Display for Coordinate {
